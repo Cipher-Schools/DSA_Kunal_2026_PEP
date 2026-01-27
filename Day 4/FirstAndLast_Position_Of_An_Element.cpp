c27d561c65ab2fcfeaa17c0805546cd89dc9d567
Kunal Jain <96196312+Kuna1jain@users.noreply.github.com>
2026-01-27 16:53:54 +0530

    Create FirstAndLast_Position_Of_An_Element.cpp

---
+++ Day 4/FirstAndLast_Position_Of_An_Element.cpp
@@ -0,0 +1,42 @@
+//First and Last Position of an element in a Sorted array
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& arr, int target) {
+        int first=-1;
+        int last=-1;
+        int low=0;
+        int high=arr.size()-1;
+        int ans=-1;
+        while(low<=high){
+        
+            int mid=low+(high-low)/2;
+            if(arr[mid]>target){
+                high=mid-1;
+            }
+            else if(arr[mid]<target){
+                low=mid+1;
+            }
+            else {
+                first=mid;
+                high=mid-1;
+            }
+        }
+        low=0;
+        high=arr.size()-1;
+        while(low<=high){
+        
+            int mid=low+(high-low)/2;
+            if(arr[mid]>target){
+                high=mid-1;
+            }
+            else if(arr[mid]<target){
+                low=mid+1;
+            }
+            else {
+                last=mid;
+                low=mid+1;
+            }
+        }
+        return {first,last};
+    }
+};
