cb85f0568ec6df958c36114fdf53445091306b13
Kunal Jain <96196312+Kuna1jain@users.noreply.github.com>
2026-02-21 15:30:36 +0530

    Create BalancedTree.cpp

---
+++ Trees/BalancedTree.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int height(TreeNode* root){
+        if(!root)return 0;
+        return 1+max(height(root->left),height(root->right));
+    }
+    bool isBalanced(TreeNode* root) {
+        if(!root)return true;
+        int lh=height(root->left);
+        int rh=height(root->right);
+        return abs(lh-rh)<2 and isBalanced(root->left) and isBalanced(root->right);
+    }
+};
